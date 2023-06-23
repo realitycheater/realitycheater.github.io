b92d44800c349b864f64e74c20ab0893534cf1f8
MattMa123 <112682932+MattMa123@users.noreply.github.com>
2023-06-23 14:17:13 +0800

    Initial commit

---
+++ test_server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+})
+
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname, "/style.css");
+});
+
+app.listen(port, () => {
+  console.log("Test server started!");
+})
